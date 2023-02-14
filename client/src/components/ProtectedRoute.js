44b9b794ebc3810b5c34c90362fa3cc8be065fcc
Rajat Bansal <rajatbansal16999@gmail.com>
2023-02-14 22:09:44 +0530

    Added NavBar without functions

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Nav } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 import { AddUserDetailForm } from "./AddUserDetailForm";
 import { EditUserDetailForm } from "./EditUserDetailForm";
 import Home from "./Home";
+import NavBar from "./NavBar";
 
 const ProtectedRoute = (props) => {
     const { user } = useUserAuth();
@@ -18,25 +19,28 @@ const ProtectedRoute = (props) => {
         return (
             <Container className="w-100 p-0">
                 <Row className="w-100 p-0">
+                    <NavBar/>
                     <Home className="w-100 p-0" />
                 </Row>
             </Container>
         );
     if (props.val == 2)
         return (
-            <Container className="w-100">
-                <Row className="w-100">
-                    <AddUserDetailForm  className="w-100"/>
-                </Row>
-            </Container>
+          <Container className="w-100">
+            <Row className="w-100">
+              <NavBar />
+              <AddUserDetailForm className="w-100" />
+            </Row>
+          </Container>
         );
     if (props.val == 3)
         return (
-            <Container className="w-100">
-                <Row className="w-100">
-                    <EditUserDetailForm  className="w-100"/>
-                </Row>
-            </Container>
+          <Container className="w-100">
+            <Row className="w-100">
+              <NavBar />
+              <EditUserDetailForm className="w-100" />
+            </Row>
+          </Container>
         );
 }
 
